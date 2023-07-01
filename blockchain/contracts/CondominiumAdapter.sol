6f8ec2742930e0d0a8e9312ba688d2f609a01fac
Plinio Mabesi <pliniomabesi@gmail.com>
2023-07-01 16:33:20 -0300

    Funções extras getManager e getQuota

--- blockchain/contracts/CondominiumAdapter.sol
+++ blockchain/contracts/CondominiumAdapter.sol
@@ -88,5 +88,13 @@ contract CondominiumAdapter {
         Lib.TransferReceipt memory receipt = implementation.transfer(topicTitle, amount);
         emit Transfer(receipt.to, receipt.amount, receipt.topic);
     }
+
+    function getManager() external view upgraded returns (address) {
+        return implementation.getManager();
+    }
+    
+    function getQuota() external view upgraded returns (uint) {
+        return implementation.getQuota();
+    }
     
 }
